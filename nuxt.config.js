50abb2999818f6b5256a13cd6837b55eeaa8b534
julianbauer <julian@overnice.com>
2019-06-25 11:31:39 +0200

    added page titles

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'overnice – brands, products & creatives',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- pages/case/_slug.vue
+++ pages/case/_slug.vue
@@ -30,7 +30,7 @@ export default {
   layout: 'content',
   head() {
     return {
-      title: this.title,
+      title: this.title + ' – overnice',
       meta: [
         // hid is used as unique identifier. Do not use `vmid` for it as it will not work
         { hid: 'description', name: this.title, content: 'My custom description..' }
