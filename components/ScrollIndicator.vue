c0c12f4c99c7f83acfef4a9dbb7b52a2415c3a8f
julianbauer <julian@overnice.com>
2019-06-26 18:35:16 +0200

    scroll indicator

---
+++ components/ScrollIndicator.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="scroll-indicator" class="scroll-indicator">
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 74">
+      <path d="M15,1L15,1c7.7,0,14,6.3,14,14v14c0,7.7-6.3,14-14,14l0,0C7.3,43,1,36.7,1,29V15C1,7.3,7.3,1,15,1z" />
+      <line x1="15" y1="11" x2="15" y2="21" />
+      <polyline class="arrow" points="25,62 15,72 5,62 " />
+    </svg>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .scroll-indicator {
+    position: fixed;
+    bottom: 10rem;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 30px;
+    height: 74px;
+    opacity: 0;
+    transition: opacity .3s ease;
+
+    svg {
+      fill: none;
+      stroke: black;
+      stroke-width: 2;
+      opacity: 0;
+      overflow: visible;
+      animation: fade-in 1s 2.25s forwards;
+    }
+
+    &.visible {
+      opacity: 1;
+    }
+  }
+
+  .arrow {
+    transform: translate3D(0,-5px,0);
+    animation: move-arrow 1.5s .75s ease infinite;
+  }
+
+  @keyframes move-arrow {
+    0% {
+      transform: translate3D(0,-5px,0);
+    }
+    50% {
+      transform: translate3D(0,5px,0);
+    }
+    100% {
+      transform: translate3D(0,-5px,0);
+    }
+  }
+
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: .25;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,7 @@
             <path d="M0,0v400h400V0H0z M96.4,219.95c-15.1,0-16.48-9.28-16.48-20.49c0-10.1,1.95-20.21,16.48-20.21 c15.08,0,16.47,9.14,16.48,20.35C112.88,209.7,110.93,219.95,96.4,219.95z M121.4,218.85l-9.4-38.5h13.3l3.7,27.8h0.1l3.6-27.8H146 l-9.4,38.5H121.4z M177.1,201.65H177v0.1h-18.9v5.1c0,4.1,1.7,5.3,3.5,5.3c2.2,0,3.6-1.5,3.6-6.8h11.6c-0.1,9.2-4.6,14.5-15.2,14.5 c-12.4,0-16.3-5.7-16.4-20.3c0-14.8,4.8-20.3,16.4-20.3c10.4,0,15.5,4.6,15.5,18.5V201.65z M202.3,191.15v-0.01 c-1.06-0.1-2.03-0.29-3.1-0.29c-4.6,0-7.2,1.8-7.2,6.9v21h-12.9H179v-38.5h12.4v6.1h0.1c1.5-4.7,5-6.2,8.7-6.2h2.1v10.99 c0.03,0,0.07,0.01,0.1,0.01H202.3z M223.8,218.85v-25.4c0-3.2-0.8-4.6-3.2-4.6c-2.4,0-3.2,1.4-3.2,4.6v25.4h-12.9v-38.5h12.4v4.7 h0.1c1.6-3.7,5-5.7,9.4-5.7c5.7,0,10.1,2.2,10.1,10.5v29H223.8z M252.2,218.85h-12.9v-38.5h12.9V218.85z M271.3,211.45 c2.4,0,3.7-1.5,3.7-6.3v-2.5h11.6c0.1,6.1-0.9,10.4-3.4,13.1c-2.5,2.8-6.5,4-11.9,4c-9.8,0-16.6-3-16.6-19.6c0-18.2,7.6-21,17.3-21 c11.5,0,14.6,6.5,14.6,16.2H275c0-3.2-0.3-5-0.8-6.2c-0.6-1.2-1.5-1.7-2.9-1.7c-2.9,0-3.7,2.5-3.7,12S268.4,211.45,271.3,211.45z  M320.1,201.65H320v0.1h-18.9v5.1c0,4.1,1.6,5.3,3.5,5.3c2.2,0,3.6-1.5,3.6-6.8h11.6c-0.3,9.2-4.6,14.5-15.2,14.5 c-12.4,0-16.4-5.7-16.4-20.3c0-14.8,4.8-20.3,16.4-20.3c10.2,0,15.5,4.6,15.5,18.5V201.65z" />
           </svg>
         </div>
+        <ScrollIndicator />
       </section>
       <IntroVideo />
       <div id="intro--text" class="intro--text">
@@ -41,6 +42,7 @@ import IntroVideo from '~/components/IntroVideo.vue'
 import ServiceCategory from '~/components/ServiceCategory.vue'
 import About from '~/components/About.vue'
 import Controls from '~/components/Layout/Controls.vue'
+import ScrollIndicator from '~/components/ScrollIndicator.vue'
 
 import serviceCategoriesData from '~/data/serviceCategories.json'
 
@@ -79,7 +81,7 @@ if (process.browser) {
 }
 
 export default {
-  components: { IntroVideo, ServiceCategory, About, Controls },
+  components: { IntroVideo, ServiceCategory, About, Controls, ScrollIndicator },
 
   data() {
     return {
@@ -91,18 +93,25 @@ export default {
   },
   methods: {
     pinContainerScene() {
+      const logoZoomDuration = 450
+
       if (window.innerWidth > 450) {
         // Zoom Logo
         const zoomLogo = this.$gsap.TweenMax.fromTo('#intro--inner', 1,
           { transform: 'scale(1)' },
           { transform: 'scale(50) translate3D(-2.8vw, 0, 0)', ease: this.$gsap.Expo.easeIn }
         )
-        const sceneZoomLogo = new this.$scrollmagic.Scene({ triggerElement: '#content', duration: 300 })
+        const sceneZoomLogo = new this.$scrollmagic.Scene({ triggerElement: '#content', duration: logoZoomDuration })
           .setTween(zoomLogo)
         this.$ksvuescr.$emit('addScene', 'sceneLogo', sceneZoomLogo)
 
+        // Hide Scroll Indicator
+        const sceneShowScrollIndicator = new this.$scrollmagic.Scene({ triggerElement: '#content', duration: 100, offset: -1 })
+          .setClassToggle('#scroll-indicator', 'visible')
+        this.$ksvuescr.$emit('addScene', 'showScrollIndicator', sceneShowScrollIndicator)
+
         // Pin Video
-        const scenePinVideo = new this.$scrollmagic.Scene({ triggerElement: '#content', duration: 450, offset: 0 })
+        const scenePinVideo = new this.$scrollmagic.Scene({ triggerElement: '#content', duration: logoZoomDuration + 150, offset: 0 })
           .setPin('#intro-video', { pushFollowers: false })
         this.$ksvuescr.$emit('addScene', 'pinVideo', scenePinVideo)
 
@@ -110,7 +119,7 @@ export default {
         const moveIntroText = this.$gsap.TweenMax.to('#intro--text', 1, {
           marginTop: '50vh', ease: this.$gsap.Expo.easeOut
         })
-        const sceneMoveIntroText = new this.$scrollmagic.Scene({ triggerElement: '#content', duration: 240, offset: 260 })
+        const sceneMoveIntroText = new this.$scrollmagic.Scene({ triggerElement: '#content', duration: 240, offset: logoZoomDuration - 40 })
           .setTween(moveIntroText)
         this.$ksvuescr.$emit('addScene', 'moveIntroText', sceneMoveIntroText)
 
@@ -119,7 +128,7 @@ export default {
           opacity: '1',
           ease: this.$gsap.Circ.easeOut
         })
-        const sceneDarkenVideo = new this.$scrollmagic.Scene({ triggerElement: '#content', duration: 200, offset: 300 })
+        const sceneDarkenVideo = new this.$scrollmagic.Scene({ triggerElement: '#content', duration: 200, offset: logoZoomDuration })
           .setTween(darkenVideo)
         this.$ksvuescr.$emit('addScene', 'darkenVideo', sceneDarkenVideo)
 
@@ -131,7 +140,7 @@ export default {
           opacity: 1,
           ease: this.$gsap.Expo.easeOut
         })
-        const sceneShrinkVideo = new this.$scrollmagic.Scene({ triggerElement: '#content', duration: 300, offset: 350 })
+        const sceneShrinkVideo = new this.$scrollmagic.Scene({ triggerElement: '#content', duration: 300, offset: logoZoomDuration + 50 })
           .setTween(shrinkVideo)
         this.$ksvuescr.$emit('addScene', 'shrinkVideo', sceneShrinkVideo)
       }
@@ -190,7 +199,7 @@ export default {
   top: 0;
   left: 0;
   width: 100%;
-  height: calc(450px + 50vh);
+  height: calc(600px + 50vh);
   margin-top: 100vh;
   box-sizing: border-box;
   padding: var(--m-spacing);
@@ -225,7 +234,7 @@ export default {
   position: relative;
   top: 50vh;
   margin-top: 1px;
-  padding-top: calc(450px + 50vh); // video scroll height + video size (minus top value)
+  padding-top: calc(600px + 50vh); // video scroll height + video size (minus top value)
 }
 
 @media (max-width: $bp-mobile) {
