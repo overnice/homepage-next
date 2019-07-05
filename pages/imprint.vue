60c4a7da85170021baed79086ef808dbfc9ed7b0
Ramicierra <rami@cierra.de>
2019-07-05 15:45:34 +0200

    imprint and privacy as md

--- pages/imprint.vue
+++ pages/imprint.vue
@@ -1,37 +1,31 @@
 <template>
-  <div>
+  <div class="content">
     <header>
-      <h1>Imprint</h1>
-      <p>
-        <span class="nowrap">Overnice Creatives GmbH i.G.</span><br>
-        <span class="nowrap">Kreuzbergstr. 28</span>
-        ∙ <span class="nowrap">10965 Berlin</span><br>
-        <span class="nowrap">hello@overnice.com</span>
+      <h1>{{ title }}</h1>
+      <p class="tags">
+        <span v-for="tag in tags" :key="tag">{{ tag }}</span>
       </p>
+      <p v-html="abstract" />
     </header>
-    <h2>
-      Liability for content
-    </h2>
-    <p>
-      We make every effort to keep the information on our Web site current, but accept no liability whatsoever for the content provided. Pursuant to §7 par. 1 of TMG (German Tele-Media Act), the law limits our responsibility as a service provider to our own content on these Web pages. According to §§8 to 10 of TMG, we are not obligated to monitor third party information provided or stored on our Web site. However, we shall promptly remove any content upon becoming aware that it violates the law. Our liability in such an instance shall commence at the time we become aware of the respective violation.
-    </p>
-    <h2>Liability for links</h2>
-    <p>
-      Our site contains links to third-party Web sites. We have no influence whatsoever on the information on these Web sites and accept no guaranty for its correctness. The content of such third-party sites is the responsibility of the respective owners/providers. At the time third-party Web sites were linked to ours, we found NO GROUNDS WHATSOEVER of any likely contravention of the law. We shall promptly delete a link upon becoming aware that it violates the law.
-    </p>
-    <h2>Copyright</h2>
-    <p>
-      The content and works provided on these Web pages are governed by the copyright laws of Germany. Duplication, processing, distribution, or any form of commercialization of such material beyond the scope of the copyright law shall require the prior written consent of its respective author or creator.
-    </p>
-    <p>
-      Source: S&K Rechtsanwälte / www.streifler.de
-    </p>
+    <div :key="$route.params.slug" v-html="body" />
   </div>
 </template>
 
 <script>
 export default {
   layout: 'content',
+  async asyncData({ params, app }) {
+    const fileContent = await import('~/static/' + app.i18n.locale + '/imprint.md')
+    const attr = fileContent.attributes
+
+    return {
+      body: fileContent.html,
+      title: attr.title,
+      abstract: attr.abstract,
+      tags: attr.tags,
+      urlTranslation: attr.urlTranslation
+    }
+  },
   head() {
     return {
       title: 'Imprint'
